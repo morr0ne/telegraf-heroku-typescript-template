ef3ee20940214b65457d37bf5fb5f04fe3527e7d
Federico Morrone <contact@morrone.dev>
2020-08-25 20:52:47 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import Telegraf from 'telegraf'
+
+const bot = new Telegraf(process.env.TOKEN || '')
+
+bot.help(ctx => {
+    ctx.reply('An helpful message')
+})
+
+if (process.env.NODE_ENV === 'production') {
+    bot.launch({
+        webhook: {
+            domain: 'http:/your-app-name.herokuapp.com/',
+            port: parseInt(process.env.PORT || '3000')
+        }
+    })
+} else {
+    bot.launch()
+}
